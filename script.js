8fcb03ceab156493c2651fc200d51f16a937ffc1
TAUREAN312 <jbotchwayowusu@gmail.com>
2024-12-20 13:23:42 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function btn(){
+  let x = document.getElementById("input").value;
+  
+  let encode = btoa(x);
+  
+  console.log(encode);
+
+document.getElementById("output").value = encode;
+}
+   
+function decode(){
+  let x = document.getElementById("input").value;
+  
+  let decode = atob(x);
+  
+  console.log(decode);
+
+document.getElementById("output").value = decode;
+}
